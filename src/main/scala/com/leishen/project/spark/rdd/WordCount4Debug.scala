c1aa2d7ea80281e7d5b628cb4ee70eacc1548894
YueYeShen <993698197@qq.com>
2016-12-05 20:52:56 +0800

    reduce端Join成功运行，添加自定义InputFormat部分代码

---
+++ src/main/scala/com/leishen/project/spark/rdd/WordCount4Debug.scala
@@ -0,0 +1,24 @@
+package com.leishen.project.spark.rdd
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by leishen on 2016/12/5 0005.
+  * 该类主要查看其源码的执行过程
+  */
+object WordCount4Debug {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local").setAppName("4debug")
+
+    val sparkContext = new SparkContext(sparkConf)
+
+    val rdd = sparkContext.textFile("L:\\word.txt")
+
+    val resultRdd = rdd.flatMap(_.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
+    resultRdd.foreach(println(_))
+
+    sparkContext.stop()
+  }
+
+}
